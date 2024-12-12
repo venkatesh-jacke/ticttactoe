364b6087257c437f43c089c6b20d9feb69a7fd68
Venkatesh Ezhumalai <54234263+venkatesh-jacke@users.noreply.github.com>
2024-12-12 12:02:46 +0530

    Update GameScreen.kt

--- app/src/main/java/com/example/tictactoe/GameScreen.kt
+++ app/src/main/java/com/example/tictactoe/GameScreen.kt
@@ -1,8 +1,94 @@
 package com.example.tictactoe
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun GameScreen(){
+fun GameScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 30.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Player 'X' : 0 ", fontSize = 16.sp)
+            Text(text = "Draw : 0 ", fontSize = 16.sp)
+            Text(text = "Player 'O' : 0", fontSize = 16.sp)
 
+        }
+        Text(
+            text = "Tic Tac Toe",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            color = Color.Red
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .clip(RoundedCornerShape(20.dp)),
+
+            contentAlignment = Alignment.Center
+        ) {
+            Board()
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Player 'X' turn", fontSize = 24.sp)
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(5.dp),
+                elevation = ButtonDefaults.buttonElevation(5.dp)
+            ) {
+                Text(text = "Play Again", fontSize = 16.sp)
+            }
+
+
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewGameScreen() {
+    GameScreen()
 }
