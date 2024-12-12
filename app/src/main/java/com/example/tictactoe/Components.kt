5f7ab3ba4bcf4c220429200da3e98973e01bb873
Venkatesh Ezhumalai <54234263+venkatesh-jacke@users.noreply.github.com>
2024-12-12 12:03:22 +0530

    Update Components.kt

--- app/src/main/java/com/example/tictactoe/Components.kt
+++ app/src/main/java/com/example/tictactoe/Components.kt
@@ -1,2 +1,202 @@
 package com.example.tictactoe
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Board() {
+    Canvas(
+        modifier = Modifier
+            .size(300.dp)
+            .padding(10.dp),
+    ) {
+        drawLine(
+            color = Color.Gray,
+            strokeWidth = 5f,
+            cap = StrokeCap.Round,
+            start = Offset(x = size.width / 3, y = 0f),
+            end = Offset(x = size.width / 3, y = size.height)
+        )
+        drawLine(
+            color = Color.Gray,
+            strokeWidth = 5f,
+            cap = StrokeCap.Round,
+            start = Offset(x = size.width * 2 / 3, y = 0f),
+            end = Offset(x = size.width * 2 / 3, y = size.height)
+        )
+        drawLine(
+            color = Color.Gray,
+            strokeWidth = 5f,
+            cap = StrokeCap.Round,
+            start = Offset(x = 0f, y = size.height / 3),
+            end = Offset(x = size.width, y = size.height / 3)
+        )
+        drawLine(
+            color = Color.Gray,
+            strokeWidth = 5f,
+            cap = StrokeCap.Round,
+            start = Offset(x = 0f, y = size.height * 2 / 3),
+            end = Offset(x = size.width, y = size.height * 2 / 3)
+        )
+    }
+}
+
+
+@Composable
+fun Circle() {
+    Canvas(
+        modifier = Modifier
+            .size(60.dp)
+            .padding(5.dp)
+    ) {
+        drawCircle(color = Color.Green, style = Stroke(width = 20f))
+    }
+}
+
+@Composable
+fun Cross() {
+    Canvas(
+        modifier = Modifier
+            .size(60.dp)
+            .padding(5.dp)
+    ) {
+        drawLine(
+            color = Color.Blue,
+            start = Offset(x = 0f, y = 0f),
+            strokeWidth = 20f,
+            end = Offset(x = size.width, y = size.height)
+        )
+        drawLine(
+            color = Color.Blue,
+            start = Offset(x = size.width, 0f),
+            strokeWidth = 20f,
+            end = Offset(x = 0f, y = size.height)
+        )
+    }
+}
+
+@Composable
+fun V1() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = size.width / 6, y = 0f),
+            end = Offset(x = size.width / 6, y = size.height)
+        )
+    }
+}
+@Composable
+fun V2() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = size.width*3  / 6, y = 0f),
+            end = Offset(x = size.width*3 / 6, y = size.height)
+        )
+    }
+}
+@Composable
+fun V3() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = size.width*5 / 6, y = 0f),
+            end = Offset(x = size.width*5 / 6, y = size.height)
+        )
+    }
+}
+
+@Composable
+fun H1() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = 0f, y =  size.width / 6),
+            end = Offset(x = size.width, y = size.width/6)
+        )
+    }
+}
+@Composable
+fun H2() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = 0f, y =  size.width*3 / 6),
+            end = Offset(x = size.width, y = size.width*3/6)
+        )
+    }
+}
+@Composable
+fun H3() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = 0f, y =  size.width*5 / 6),
+            end = Offset(x = size.width, y = size.width*5/6)
+        )
+    }
+}
+
+@Composable
+fun D1() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = 0f, y =  0f),
+            end = Offset(x = size.height, y = size.width)
+        )
+    }
+}
+
+@Composable
+fun D2() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawLine(
+            color = Color.Red,
+            strokeWidth = 10f,
+            cap = StrokeCap.Round,
+            start = Offset(x = size.width, y = 0f),
+            end = Offset(x = 0f, y = size.height)
+        )
+    }
+}
+
+
+
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewComposable() {
+    H1()
+    H2()
+    H3()
+    V1()
+    V2()
+    V3()
+    D1()
+    D2()
+}
